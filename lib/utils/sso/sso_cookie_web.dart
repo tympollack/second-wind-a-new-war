f8b197293d56312e3c813565b10f9d6f74fea373
Tymz Pollack <tympollack@gmail.com>
2026-07-11 16:29:56 -0400

    ignore error

--- lib/utils/sso/sso_cookie_web.dart
+++ lib/utils/sso/sso_cookie_web.dart
@@ -1,4 +1,4 @@
-// ignore: deprecated_member_use
+// ignore: deprecated_member_use, avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
 Future<String?> getSsoSessionJson(String projectRef) async {
